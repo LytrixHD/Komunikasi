e8ab2506f7057a8e1e50f90b4beb62b28f093404
LytrixHD <lytrixhd@gmail.com>
2022-06-09 14:23:24 +0200

    Added Semicolon at forms/index.ts

--- src/app/core/forms/index.ts
+++ src/app/core/forms/index.ts
@@ -1,4 +1,4 @@
 export {LoginFormComponent as LoginForm} from './auth/login-form/login-form.component';
-export {RegisterFormComponent as RegisterForm} from './auth/register-form/register-form.component'
+export {RegisterFormComponent as RegisterForm} from './auth/register-form/register-form.component';
 export {ChangePasswordFormComponent as ChangePWForm} from './usermanagement/change-password-form/change-password-form.component';
 export {DeleteUserFormComponent as DeleteUserForm} from './usermanagement/delete-user-form/delete-user-form.component';
